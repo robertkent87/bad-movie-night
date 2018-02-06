650d5149b4a9387a21b1a31dd1efb3496f31708b
Robert Kent <robert.kent87@gmail.com>
2018-02-06 14:21:55 +0000

    Added footer to mockups

--- wp-content/themes/badmovienight/html/index.php
+++ wp-content/themes/badmovienight/html/index.php
@@ -178,6 +178,95 @@
             <!-- /Movie listing -->
         </div>
     </div>
+    <footer class="bg-dark text-white">
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Latest News</h3>
+                    <ul class="list-unstyled">
+                        <li class="media">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                        <li class="media my-4">
+                            <img class="mr-3" src="http://via.placeholder.com/64x64?text=Avatar"
+                                 alt="Generic placeholder image">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Recent Tweets</h3>
+                    <ul class="list-unstyled">
+                        <li class="media">
+                            <img class="mr-3" src="http://via.placeholder.com/64x64?text=Avatar"
+                                 alt="Generic placeholder image">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                        <li class="media my-4">
+                            <img class="mr-3" src="http://via.placeholder.com/64x64?text=Avatar"
+                                 alt="Generic placeholder image">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                        <li class="media">
+                            <img class="mr-3" src="http://via.placeholder.com/64x64?text=Avatar"
+                                 alt="Generic placeholder image">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">List-based media object</h5>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                sollicitudin.
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Get In Touch</h3>
+                    <form action="">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <p>&copy; <?= date('Y') ?> <a href="https://robert-kent.com" target="_blank">Robert Kent</a>. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/script.js"></script>
 </body>
