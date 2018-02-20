68a07bf18228fc0c788c9483d57102593175f188
Robert Kent <robert.kent87@gmail.com>
2018-02-20 12:59:57 +0000

    Added news to site footer

--- wp-content/themes/badmovienight/footer.php
+++ wp-content/themes/badmovienight/footer.php
@@ -16,48 +16,44 @@
 <footer class="bg-black text-white mt-5">
     <div class="container pt-4">
         <div class="row">
-<!--            <div class="col-md-4">-->
-<!--                <h3>Latest News</h3>-->
-<!--                <div class="media">-->
-<!--                    <div class="media-body">-->
-<!--                        <h5 class="mt-0 mb-1">List-based media object</h5>-->
-<!--                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante-->
-<!--                        sollicitudin.-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="media my-4">-->
-<!--                    <img class="mr-3" src="http://via.placeholder.com/64x64?text=Avatar"-->
-<!--                         alt="Generic placeholder image">-->
-<!--                    <div class="media-body">-->
-<!--                        <h5 class="mt-0 mb-1">List-based media object</h5>-->
-<!--                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante-->
-<!--                        sollicitudin.-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="media">-->
-<!--                    <div class="media-body">-->
-<!--                        <h5 class="mt-0 mb-1">List-based media object</h5>-->
-<!--                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante-->
-<!--                        sollicitudin.-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-            <div class="col-md-6">
-                <?php if (is_active_sidebar('footer_column_1')): ?>
-                    <?php dynamic_sidebar('footer_column_1'); ?>
-                <?php endif; ?>
+            <div class="col-md-4">
+                <h3>Latest News</h3>
+
+				<?php if (have_posts()): ?>
+					<?php while (have_posts()) : the_post(); ?>
+                        <div class="media my-4">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1"><a href="<?php print get_field('link') ?>" target="_blank" class=""><?php the_title(); ?></a></h5>
+                                <p class="news-date">
+                                    <small><?php print get_the_date('l, F j Y') ?></small>
+                                </p>
+                            </div>
+                        </div>
+					<?php endwhile; // End of the loop.?>
+				<?php else: ?>
+                    <p>No news found</p>
+				<?php endif; ?>
+            </div>
+            <div class="col-md-4">
+				<?php if (is_active_sidebar('footer_column_1')): ?>
+					<?php dynamic_sidebar('footer_column_1'); ?>
+				<?php endif; ?>
             </div>
 
-            <div class="col-md-6" id="footer-contact">
+            <div class="col-md-4" id="footer-contact">
                 <h3>Get In Touch</h3>
-	            <?php echo do_shortcode('[contact-form-7 id="55" title="Contact form 1" html_id="contact-form"]'); ?>
+				<?php echo do_shortcode('[contact-form-7 id="55" title="Contact form 1" html_id="contact-form"]'); ?>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="container">
-            <p><small>&copy; <?= date('Y') ?> <a href="https://robert-kent.com" target="_blank">Robert Kent</a>. All rights
-                    reserved.</small></p>
+            <p>
+                <small>&copy; <?= date('Y') ?> <a href="https://robert-kent.com" target="_blank">Robert Kent</a>. All
+                    rights
+                    reserved.
+                </small>
+            </p>
         </div>
     </div>
 </footer>
