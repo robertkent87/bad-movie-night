1677799e64fc91de8393cb20bb928371ce5265e9
Robert Kent <robert.kent87@gmail.com>
2018-02-22 15:40:13 +0000

    CSS cache string

--- wp-content/themes/badmovienight/functions.php
+++ wp-content/themes/badmovienight/functions.php
@@ -141,7 +141,7 @@
 //        wp_enqueue_style('signika', 'https://fonts.googleapis.com/css?family=Signika:400,700');
         wp_enqueue_style('roboto', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:700');
 
-		wp_enqueue_style('badmovienight-style', get_stylesheet_uri());
+		wp_enqueue_style('badmovienight-style', get_stylesheet_uri(), [], '20180222');
 
 		wp_deregister_script('jquery');
 		wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.3.1.min.js', array(), '3.3.1');
